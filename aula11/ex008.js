544335c0f1b80d39f06abb118d6c6e07f225d174
Vinicius Giovanne Paganucci Vidotto <98669129+vinipaganucci@users.noreply.github.com>
2022-02-28 00:39:54 +0000

    update

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 88.2
+console.log(`A velocidade do seu carro é de ${vel}Km/h.`)
+
+if(vel > 120){
+    console.log('Você está acima do limite de velocidade! MULTADO!')
+}
+
+else{
+    console.log(`\nDirija usando sempre usando cinto de segurança!`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+console.log(`Vivendo no/na ${pais} você é:`)
+if(pais == 'Brasil'){
+    console.log('Brasileiro')
+}
+
+else{
+    console.log('Estrangeiro')
+}
