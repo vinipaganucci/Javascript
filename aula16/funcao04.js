1999f9562613d29d4e7835dec3d3dcc0307bc6c3
Vinicius Giovanne Paganucci Vidotto <98669129+vinipaganucci@users.noreply.github.com>
2022-03-04 22:43:50 +0000

    update

---
+++ aula16/funcao04.js
@@ -0,0 +1,10 @@
+function fator(n){
+    let fat = 1
+    for(let c = n; c > 1; c--){
+        fat *= c
+    }
+
+    return fat
+}
+
+console.log(fator(5))
---
+++ aula16/funcao05.js
@@ -0,0 +1,10 @@
+//RECURSIVIDADE (REUTILIZAR OUTRA FUNÇÕES DENTRO DE OUTRA FUNÇÃO)
+function fatorial(n){
+    if(n == 1){
+        return 1
+    } else{
+        return n*fatorial(n-1)
+    }
+}
+
+console.log(fatorial(5))
