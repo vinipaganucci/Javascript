cbe963641e5940850cb722ff90e12be4d6ded46d
Vinicius Giovanne Paganucci Vidotto <98669129+vinipaganucci@users.noreply.github.com>
2022-03-04 20:22:57 +0000

    update

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5,7,3,4,6,8,5]
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor é ${num[0]}`)
+let pos = num.indexOf(4)
+console.log(pos)
+
---
+++ aula15/vetornatela.js
@@ -0,0 +1,9 @@
+/*
+for(var i = 0;i <= num.length;i++){
+    console.log(`A posição ${i} tem o valor ${num[i]}`)
+}
+*/
+let num = [5,7,3,4,6,8,5,9]
+for(let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
