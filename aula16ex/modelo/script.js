e90a36113c5a9ad13dc6eacaa7efdd916bc74482
Vinicius Giovanne Paganucci Vidotto <98669129+vinipaganucci@users.noreply.github.com>
2022-03-12 17:26:05 +0000

    update

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -60,7 +60,7 @@ function finalizar() {
         res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
         res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
         res.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
-        res.innerHTML += `<p>A média dos valores é ${media}</p>`
         
     }
+    res.innerHTML += `<p>A média dos valores é ${media}</p>`
 }
